35de00952dc3b3bb296300216462d33e2933f801
laitsky <vincent.diamond15@gmail.com>
2020-02-02 11:48:46 +0700

    jawaban1.php done

---
+++ modul01/PemwebWeek2/jawaban1.php
@@ -0,0 +1,24 @@
+<?php
+$negara = array(
+    "India" => array(
+        "ibuKota" => "New Delhi",
+        "kodeTel" => "91",
+        "mataUang" => "INR"
+    ),
+    "Indonesia" => array(
+        "ibuKota" => "Jakarta",
+        "kodeTel" => "62",
+        "mataUang" => "IDR",
+    ),
+    "Japan" => array(
+        "ibuKota" => "Tokyo",
+        "kodeTel" => "81",
+        "mataUang" => "JPY"
+    )
+);
+
+foreach ($negara as $key => $n) {
+    echo "<strong><em>" . $n["ibuKota"] . "</em></strong>" . " is capital city of " . "<strong>" . $key . "</strong>" . ". " . "<u>" . $key . "'s calling code is " . $n["kodeTel"] . ' and has "' . $n["mataUang"] . '" as currency code.' . "</u>";
+    echo "<br>";
+}
+
