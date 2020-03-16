145990e7d44a372a914350e8ec5c2349d5540c9a
laitsky <vincent.diamond15@gmail.com>
2020-03-16 10:32:28 +0700

    uts pemweb login session fix

--- UTS2020_PemWeb-CL_27047_VincentDiamond_Soal04/index.php
+++ UTS2020_PemWeb-CL_27047_VincentDiamond_Soal04/index.php
@@ -1,5 +1,4 @@
 <?php
 error_reporting(0);
 require_once "include/header.php";
-destroy_session();
 require_once "view/login.php";
--- UTS2020_PemWeb-CL_27047_VincentDiamond_Soal04/view/login.php
+++ UTS2020_PemWeb-CL_27047_VincentDiamond_Soal04/view/login.php
@@ -3,6 +3,13 @@
 <div class="container pt-4" style="max-width: 600px;">
     <div id="login-card" class="card-shadow" style="padding: 1.5em;">
         <h2 class="text-center" style="font-weight: bolder;">Masuk ke UShop</h2>
+        <?php
+        if (isset($_SESSION['error'])) {
+            ?>
+            <div class="alert alert-danger" role="alert">
+                ID atau Password salah!
+            </div>
+        <?php } ?>
         <form action="controller/check_login.php" method="post">
             <div class="form-group">
                 <label for="user_id">Masukkan ID</label>
@@ -11,16 +18,6 @@
             <div class="form-group">
                 <label for="password">Masukkan Password</label>
                 <input type="password" id="password" name="password" class="form-control" placeholder="Password">
-            </div>
-            <div class="form-group">
-                <div id="msg" class="form-group" style="color: red;">
-                    <?php
-                    if (isset($_SESSION['error'])) {
-                        $error = $_SESSION['error'];
-                        echo $error;
-                    }
-                    ?>
-                </div>
             </div>
             <button class="btn btn-primary btn-block mb-3" id="button" name="login_submit"><i
                         class="las la-sign-in-alt"></i>Login
